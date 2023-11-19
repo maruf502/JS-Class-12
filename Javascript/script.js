5afedf053c6e29323bd065e322325aa5d3196bc4
Maruf Khan <marufatrai12@gmail.com>
2023-11-19 10:17:33 +0600

    JS-Class-12

---
+++ Javascript/script.js
@@ -0,0 +1,110 @@
+
+
+
+
+    // JavaScript Relational Operator   >, >=, <, <=, ==, ===, !=, !==
+
+    // Javascript Logical Operator  &&, ||, !
+
+/*
+
+    var x = 50;
+    var y = 20;
+
+    document.write(x>y); // output true
+
+*/
+
+/*
+
+    var x = 30 ;
+    var y = 10 ;
+
+    document.write(x>=y);   //  Output true 
+
+*/
+
+/*
+
+    var x = 70;
+    var y = 50;
+
+    document.write(x<y);    // OutPut false
+
+*/
+
+
+/*
+
+    var x = 30;
+    var y = 20;
+
+    document.write(x<=y);   //  output false 
+
+*/
+
+/*
+
+    var x = 50;
+    var y = 50;
+
+    document.write(x==y);   // output true
+
+*/
+
+
+/*
+
+    var x = 20;
+    var y = "20";
+
+    document.write(x===y);  //   outPut false
+
+*/
+
+/*
+
+    var x = 20;
+    var y = 15;
+
+    document.write(x!=y);   //  output true
+
+*/
+
+/*
+
+    var x = 15;
+    var y = "15";
+
+    document.write(x!==y);  //  output true
+
+*/
+
+                    // Javascript Logical Operator  &&, ||, !
+
+
+
+/*            
+
+    var x = 10;
+    var y = 5;
+    var z = 2;
+
+    document.write(x>y && x<z);  // output false
+
+*/
+
+/*
+
+    var x = 20;
+    var y = 15;
+    var z = 10;
+
+    document.write(x>y || x<z); // output true
+
+*/
+
+
+    //  document.write(!(25>15));   // output false;
+
+    document.write(!(15>25));   //  output true
